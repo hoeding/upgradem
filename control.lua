df3a7fe696f7493b7ff6a64b137104aff5a91d31
hoeding <hoeding@gmail.com>
2024-04-04 20:25:19 -0600

    add /create_surfaces command

--- control.lua
+++ control.lua
@@ -1,22 +1,32 @@
 
 local tile_size = { x = 32  , y = 0 }
 
+local upgrade_table = {
+}
 
-script.on_event(defines.events.on_built_entity,
-  function(event)
+Number_of_surfaces = 0
 
-    if event.item ~= nil then
-      local old_entity = event.created_entity
+commands.add_command("create-surfaces", "Create n surfaces, where syntax is /create-surfaces n", function(command)
+  if command.parameter ~= nil then
+    for iter = 0, tonumber(command.parameter), 1 do
+      game.create_surface("upgradem-" .. tostring(iter + 1))
+    end
+    Number_of_surfaces = n
+  end
+  game.print(command.parameter)
+  end
+)
 
-      
-      if old_entity.name == "assembling-machine-2" then
-        if game.surfaces[1].find_non_colliding_position ("assembling-machine-3", {x = old_entity.position.x + tile_size.x, y = old_entity.position.y + tile_size.y}, 1, 1,true) 
-        ~= nil then
-          local upgraded_assembler = game.surfaces[1].create_entity({
-            name= "assembling-machine-3",
-            position = {x = old_entity.position.x + tile_size.x, y = old_entity.position.y + tile_size.y},
-            force = old_entity.force
-          })
+script.on_event(defines.events.on_built_entity,function(event)
+  if event.item ~= nil then
+    local old_entity = event.created_entity
+    if old_entity.name == "assembling-machine-2" then
+      if game.surfaces[1].find_non_colliding_position ("assembling-machine-3", {x = old_entity.position.x + tile_size.x, y = old_entity.position.y + tile_size.y}, 1, 1,true) ~= nil then
+        local upgraded_assembler = game.surfaces[1].create_entity({
+          name= "assembling-machine-3",
+          position = {x = old_entity.position.x + tile_size.x, y = old_entity.position.y + tile_size.y},
+          force = old_entity.force
+        })
         end
       end
     end
@@ -28,8 +38,6 @@ script.on_event(defines.events.on_pre_player_mined_item,
     game.print("hello mined event")
   end  
 )
-
-
 
 script.on_event(defines.events.on_robot_built_entity,
   function(event)
